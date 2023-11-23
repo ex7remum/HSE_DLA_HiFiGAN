573b970c6ef527ae724bbfd04f63ff8d1b986fe0
Yuriy Maksyuta <yura-maksuta@mail.ru>
2023-11-23 17:56:40 +0300

    Fix test

--- test.py
+++ test.py
@@ -57,6 +57,7 @@ def main(config, out_dir):
             "A defibrillator is a device that gives a high energy electric shock to the heart of someone who is in cardiac arrest",
             "Massachusetts Institute of Technology may be best known for its math, science and engineering education",
             "Wasserstein distance or Kantorovich Rubinstein metric is a distance function defined between probability distributions on a given metric space",
+            "Vsem privet, s vami professor moriarti"
         ]
 
     test_data = defaultdict(list)
