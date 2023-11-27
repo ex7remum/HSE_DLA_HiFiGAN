fb64f5a67c078ce4b44a946a0591503d1bca1d4b
Yuriy Maksyuta <yura-maksuta@mail.ru>
2023-11-27 22:50:57 +0300

    Fix generator typo

--- hw_nv/model/Generator.py
+++ hw_nv/model/Generator.py
@@ -77,7 +77,6 @@ class Generator(nn.Module):
             self.d_r = d_r
 
         self.h_channels = h
-        self.k_u = k_u
         self.enter_conv = weight_norm(nn.Conv1d(in_channels=MelSpectrogramConfig.n_mels, out_channels=h,
                                       kernel_size=7, stride=1, padding=3))
         self.blocks = nn.ModuleList()
